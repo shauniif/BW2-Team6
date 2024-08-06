7bb8ae26e1d6604c039354e450eea481577bcda0
shauniif <francescavescio12@gmail.com>
2024-08-06 11:54:00 +0200

    inserite view Recover

---
+++ Controllers/RecoverController.cs
@@ -0,0 +1,66 @@
+﻿using BW2_Team6.Models;
+using BW2_Team6.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BW2_Team6.Controllers
+{
+    public class RecoverController : Controller
+    {
+        private readonly IRecoverService _recoverSvc;
+
+        public RecoverController(IRecoverService recoverService)
+        {
+            _recoverSvc = recoverService;
+        }
+        public async Task<IActionResult> AllRecovers()
+        {
+            var recovers = await _recoverSvc.GetAll();
+            return View(recovers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int animalId, RecoverViewModel entity)
+        {
+            if (ModelState.IsValid)
+            {
+                await _recoverSvc.Create(animalId, entity);
+                return RedirectToAction("AllRecovers", "Recover");
+            } else
+            {
+                return View(entity);
+            }
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var recover = await _recoverSvc.Read(id);
+            return View(recover);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, RecoverViewModel entity)
+        {
+            var recover = await _recoverSvc.Update(id, entity);
+            return View(recover);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var recover = await _recoverSvc.Read(id);
+            return View(recover);
+        }
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recover = await _recoverSvc.Delete(id);
+            return RedirectToAction("AllRecovers", "Recover");
+        }
+    }
+}
---
+++ Models/RecoverViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BW2_Team6.Models
+{
+    public class RecoverViewModel
+    {
+        public IFormFile Image { get; set; }
+    }
+}
